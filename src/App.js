ba893e595c07c3288f56856eb5015ea58e3f850b
Ascani Carlo <carlo.ratm@gmail.com>
2019-12-31 16:44:09 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,144 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { ReactComponent as Animals } from "./animals/svg/sprite.symbol.svg";
+import "./App.css";
+
+const Timer = () => <section className="Timer">timer</section>;
+
+const Cell = ({ val, current, active, index }) => {
+  const currentClass = current ? "is-current" : "";
+  const activeClass = active ? "is-active" : "";
+  return (
+    <div className={`Cell Cell--${val} ${currentClass} ${activeClass}`}>
+      <svg viewBox="0 0 100 100">
+        <use xlinkHref={`#${val}`} />
+      </svg>
+    </div>
+  );
+};
+
+const Board = ({ board, current, active }) => (
+  <section className="Board">
+    {board.map((c, i) => (
+      <Cell val={c} key={i} current={current === i} active={active} index={i} />
+    ))}
+  </section>
+);
+
+const Main = ({ children }) => <section className="Main">{children}</section>;
+
+const OSD = () => <section className="OSD">OSD</section>;
+
+const randomCell = () => Math.floor(Math.random() * 8 + 1);
+
+const randomBoard = () => Array.from(new Array(64), randomCell);
+
+export const arrayEquals = (arr1, arr2) => {
+  if (!arr1 || !arr2 || arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) if (arr1[i] !== arr2[i]) return false;
+  return true;
+};
+
+export const findSeq = arr => {
+  if (arr.length !== 8 || arr.includes(undefined)) return [];
+
+  let result = [];
+  let i = 0;
+  while (i < 8) {
+    let count = 1;
+    while (arr[i] === arr[i + count]) count++;
+    if (count >= 3)
+      result.push(
+        Array(count)
+          .fill(i)
+          .map((i, idx) => i + idx)
+      );
+    i += count;
+  }
+  return result;
+};
 
 function App() {
+  const [board, setBoard] = useState(randomBoard());
+  const [current, setCurrent] = useState(0);
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {}, []);
+
+  const row = () => Math.floor(current / 8);
+
+  const col = () => Math.floor(current % 8);
+
+  const rowValues = r => [...Array(8).keys()].map(off => board[r + off]);
+
+  const colValues = c => [...Array(8).keys()].map(off => board[c + off * 8]);
+
+  const swapCell = delta => {
+    const newBoard = [...board];
+    newBoard[current] = board[current + delta];
+    newBoard[current + delta] = board[current];
+    return newBoard;
+  };
+
+  const updateBoard = () => {
+    if (board.includes(undefined)) return null;
+
+    const newBoard = [...board];
+    for (let r = 0; r < 64; r += 8)
+      findSeq(rowValues(r))
+        .map(s => s.map(off => r + off))
+        .forEach(s => s.forEach(i => (newBoard[i] = randomCell())));
+    for (let c = 0; c < 8; c++)
+      findSeq(colValues(c))
+        .map(s => s.map(off => c + off * 8))
+        .forEach(s => s.forEach(i => (newBoard[i] = randomCell())));
+    return arrayEquals(board, newBoard) ? null : newBoard;
+  };
+
+  useEffect(() => {
+    const newBoard = updateBoard();
+    if (newBoard) setBoard(newBoard);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [board]);
+
+  const moveBy = delta => {
+    if (active) {
+      setBoard(swapCell(delta));
+      setActive(false);
+    }
+    setCurrent(current + delta);
+  };
+
+  const onKey = event => {
+    switch (event.keyCode) {
+      case 32:
+        setActive(!active);
+        break;
+      case 37:
+        if (col() - 1 >= 0) moveBy(-1);
+        break;
+      case 38:
+        if (row() - 1 >= 0) moveBy(-8);
+        break;
+      case 39:
+        if (col() + 1 <= 7) moveBy(1);
+        break;
+      case 40:
+        if (row() + 1 <= 7) moveBy(8);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="Reanimact" tabIndex="0" onKeyUp={onKey}>
+      <Main>
+        <Timer />
+        <Board board={board} current={current} active={active} />
+      </Main>
+      <OSD />
+      <Animals style={{ display: "none" }} />
+    </section>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,47 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { findSeq, arrayEquals } from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("Finds a sequence of 3 or more consecutive numbers in an array of 8 elements", () => {
+  let arr = [1, 4, 5, 6, 6, 7];
+  expect(findSeq(arr)).toEqual([]);
+
+  arr = [2, undefined, 5, 6, 6, 6, 1, 1];
+  expect(findSeq(arr)).toEqual([]);
+
+  arr = [1, 4, 5, 6, 6, 7, 3, 1];
+  expect(findSeq(arr)).toEqual([]);
+
+  arr = [2, 4, 5, 6, 6, 6, 1, 1];
+  expect(findSeq(arr)).toEqual([[3, 4, 5]]);
+
+  arr = [4, 4, 4, 4, 7, 7, 7, 7];
+  expect(findSeq(arr)).toEqual([[0, 1, 2, 3], [4, 5, 6, 7]]);
+
+  arr = [4, 4, 4, 4, 4, 7, 4, 4];
+  expect(findSeq(arr)).toEqual([[0, 1, 2, 3, 4]]);
+
+  arr = [1, 2, 2, 2, 4, 2, 2, 2];
+  expect(findSeq(arr)).toEqual([[1, 2, 3], [5, 6, 7]]);
+
+  arr = [2, 2, 2, 2, 2, 8, 8, 8];
+  expect(findSeq(arr)).toEqual([[0, 1, 2, 3, 4], [5, 6, 7]]);
+});
+
+it("Returns true if two arrays contain the same elements", () => {
+  let arr1 = [2, 1, 2, 2, 7, 8, 8, 8];
+  let arr2 = [2, 1, 2, 2, 7, 8, 8, 8];
+  expect(arrayEquals(arr1, arr2)).toEqual(true);
+
+  arr1 = [];
+  arr2 = [];
+  expect(arrayEquals(arr1, arr2)).toEqual(true);
+});
+
+it("Returns false if two arrays don't contain the same elements", () => {
+  let arr1 = [2, 1, 2, 2, 7, 3, 8, 8];
+  let arr2 = [2, 1, 2, 3, 7, 8, 8, 8];
+  expect(arrayEquals(arr1, arr2)).toEqual(false);
+
+  arr1 = [1];
+  arr2 = [];
+  expect(arrayEquals(arr1, arr2)).toEqual(false);
 });
