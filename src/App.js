23c5ba7da6f14129e7939de67079347d8593ca9a
Ascani Carlo <carlo.ratm@gmail.com>
2020-01-05 18:03:10 +0100

    Add some help text

--- src/App.js
+++ src/App.js
@@ -70,6 +70,10 @@ const OSD = ({ score, level, onplay, playing }) => (
   <section className="OSD">
     <p className="OSD-line">Level: {level}</p>
     <p className="OSD-line">Score: {score}</p>
+    <p className="OSD-line--help">
+      Arrow keys to move, spacebar and arrow keys to swap hippo in any direction.
+      Align 3 or more hippos of the same colors to make them disappear.
+    </p>
     <p className="OSD-line--bottom">
       <button onClick={e => onplay()} className="OSD-audio">
         <svg viewBox="0 0 100 100">
